fa19d14e9bd426bea8c7c8b56dbfdc95dd435b7d
TactikalMakiRoll <sagridin@gmail.com>
2023-03-07 23:06:47 +0200

    feat: implemented gradient random colors for album covers, continuing markup for main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex">
     <NavBar></NavBar>
+    <!-- Central part -->
     <main class="grow py-10 px-8">
+      <!-- Search bar and profile -->
       <div class="flex gap-9">
         <input
           class="grow rounded-full bg-[rgb(28,27,28)] px-9 py-4 text-[rgb(187,187,187)] placeholder:text-[rgb(119,119,119)]"
@@ -18,31 +20,130 @@
           />
         </div>
       </div>
+      <!-- Albums -->
       <section class="mt-16">
         <h2 class="text-lg font-bold">Recently Listened Albums</h2>
         <div class="mt-8 flex gap-5">
-          <div class="aspect-[1.4] grow overflow-hidden rounded-lg bg-blue-500">
-            <div class="mt-auto bg-black/30 py-6 pl-6 backdrop-blur-sm">
+          <div
+            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
+            :style="createGradientStops()"
+          >
+            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
               <h3 class="font-bold">Hip-Hop</h3>
-              <p class="text-[rgb(190,186,197)]">120 new songs</p>
+              <p class="text-[rgb(214,214,214)]">120 new songs</p>
             </div>
           </div>
-          <div class="aspect-[1.4] grow overflow-hidden rounded-lg bg-blue-500">
-            <div class="mt-auto bg-black/30 py-6 pl-6 backdrop-blur-sm">
+          <div
+            :style="createGradientStops()"
+            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
+          >
+            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
               <h3 class="font-bold">Indie</h3>
-              <p class="text-[rgb(190,186,197)]">34 new songs</p>
+              <p class="text-[rgb(214,214,214)]">34 new songs</p>
             </div>
           </div>
-          <div class="aspect-[1.4] grow overflow-hidden rounded-lg bg-blue-500">
-            <div class="mt-auto bg-black/30 py-6 pl-6 backdrop-blur-sm">
+          <div
+            :style="createGradientStops()"
+            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
+          >
+            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
               <h3 class="font-bold">Pop</h3>
-              <p class="text-[rgb(190,186,197)]">100 new songs</p>
+              <p class="text-[rgb(214,214,214)]">100 new songs</p>
             </div>
           </div>
-          <div class="aspect-[1.4] grow overflow-hidden rounded-lg bg-blue-500">
-            <div class="mt-auto bg-black/30 py-6 pl-6 backdrop-blur-sm">
+          <div
+            :style="createGradientStops()"
+            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
+          >
+            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
               <h3 class="font-bold">Rap</h3>
-              <p class="text-[rgb(190,186,197)]">49 new songs</p>
+              <p class="text-[rgb(214,214,214)]">49 new songs</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Trending songs -->
+      <section class="mt-16">
+        <h2 class="text-lg font-bold">Trending Right Now</h2>
+        <div>
+          <div>
+            <div>
+              <h4>01</h4>
+              <img src="images/trackCover.jpg" alt="song image" />
+            </div>
+            <div>
+              <h3>Millions</h3>
+              <p>Always never</p>
+            </div>
+            <div>
+              <p>Always never</p>
+              <p>8.069.542</p>
+              <p>3:58</p>
+              <button><img src="icons/like.svg" alt="like image" /></button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4>02</h4>
+              <img src="images/trackCover.jpg" alt="song image" />
+            </div>
+            <div>
+              <h3>If You Let Me</h3>
+              <p>Alina Baraz</p>
+            </div>
+            <div>
+              <p>SunBeam</p>
+              <p>1.029.293</p>
+              <p>2:15</p>
+              <button><img src="icons/like.svg" alt="like image" /></button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4>03</h4>
+              <img src="images/trackCover.jpg" alt="song image" />
+            </div>
+            <div>
+              <h3>It's everyday bro</h3>
+              <p>Logan Paul</p>
+            </div>
+            <div>
+              <p>Logan TT</p>
+              <p>5.000.000</p>
+              <p>2:40</p>
+              <button><img src="icons/like.svg" alt="like image" /></button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4>04</h4>
+              <img src="images/trackCover.jpg" alt="song image" />
+            </div>
+            <div>
+              <h3>It's everyday bro</h3>
+              <p>Logan Paul</p>
+            </div>
+            <div>
+              <p>Logan TT</p>
+              <p>5.000.000</p>
+              <p>2:40</p>
+              <button><img src="icons/like.svg" alt="like image" /></button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4>05</h4>
+              <img src="images/trackCover.jpg" alt="song image" />
+            </div>
+            <div>
+              <h3>It's everyday bro</h3>
+              <p>Logan Paul</p>
+            </div>
+            <div>
+              <p>Logan TT</p>
+              <p>5.000.000</p>
+              <p>2:40</p>
+              <button><img src="icons/like.svg" alt="like image" /></button>
             </div>
           </div>
         </div>
@@ -52,29 +153,44 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
 import NavBar from '../components/UI/NavBar.vue';
 
-const createAlbumBG = computed(() => {
+function createGradientStops() {
+  // define possible gradient directions
   let gradientDirections = [
-    'bg-gradient-to-t',
-    'bg-gradient-to-tr',
-    'bg-gradient-to-r',
-    'bg-gradient-to-br',
-    'bg-gradient-to-b',
-    'bg-gradient-to-bl',
-    'bg-gradient-to-l',
-    'bg-gradient-to-tl'
+    'to top',
+    'to top right',
+    'to right',
+    'to bottom right',
+    'to bottom',
+    'to bottom left',
+    'to left',
+    'to top left'
   ];
+
+  // prepare color variables
   let hexValues = '0123456789abcdef';
   let fromColor = '#';
   let viaColor = '#';
   let toColor = '#';
 
+  // randomly generate color using hexValues string with possible hex symbols
   for (let i = 0; i < 6; i++) {
     fromColor += hexValues.charAt(Math.floor(Math.random() * hexValues.length));
     viaColor += hexValues.charAt(Math.floor(Math.random() * hexValues.length));
     toColor += hexValues.charAt(Math.floor(Math.random() * hexValues.length));
   }
-});
+
+  // return the object to be used in styling album cover -> using tailwind values
+  return {
+    '--tw-gradient-from': fromColor,
+    '--tw-gradient-via': viaColor,
+    '--tw-gradient-to': toColor,
+    '--tw-gradient-stops': 'var(--tw-gradient-from), var(--tw-gradient-to), var(--tw-gradient-to)',
+    'background-image':
+      'linear-gradient(' +
+      gradientDirections[Math.floor(Math.random() * 7)] +
+      ', var(--tw-gradient-stops))'
+  };
+}
 </script>
