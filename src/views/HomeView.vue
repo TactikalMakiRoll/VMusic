c159f00c315b58aefbb0a2801eb0ef009cd4c731
TactikalMakiRoll <sagridin@gmail.com>
2023-03-29 15:37:14 +0300

    feat: scrollable elements now have automatic fade in/out depending on how much it is scrolled

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,104 +25,25 @@
         <h2 class="text-xl font-bold capitalize">Recently Listened Albums</h2>
         <div class="mt-8 flex gap-5">
           <div
-            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
+            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90 hover:cursor-pointer"
             :style="createGradientStops()"
+            v-for="n in 4"
           >
             <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
               <h3 class="font-bold">Hip-Hop</h3>
               <p class="text-[rgb(214,214,214)]">120 new songs</p>
             </div>
           </div>
-          <div
-            :style="createGradientStops()"
-            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
-          >
-            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
-              <h3 class="font-bold">Indie</h3>
-              <p class="text-[rgb(214,214,214)]">34 new songs</p>
-            </div>
-          </div>
-          <div
-            :style="createGradientStops()"
-            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
-          >
-            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
-              <h3 class="font-bold">Pop</h3>
-              <p class="text-[rgb(214,214,214)]">100 new songs</p>
-            </div>
-          </div>
-          <div
-            :style="createGradientStops()"
-            class="flex aspect-[1.4] grow flex-col overflow-hidden rounded-lg brightness-90"
-          >
-            <div class="grow-1 mt-auto bg-black/40 py-6 pl-6 backdrop-blur-3xl">
-              <h3 class="font-bold">Rap</h3>
-              <p class="text-[rgb(214,214,214)]">49 new songs</p>
-            </div>
-          </div>
         </div>
       </section>
       <!-- Trending songs -->
-      <section
-        class="mt-16 min-h-0 flex-1 [mask-image:linear-gradient(180deg,black_0%,black_75%,transparent_100%)]"
-      >
+      <section class="mt-16 min-h-0 flex-1">
         <h2 class="text-xl font-bold capitalize">Trending Right Now</h2>
-        <div class="mt-9 max-h-[calc(100%-5rem)] snap-y snap-end snap-always overflow-y-auto">
-          <div class="flex items-center">
-            <div class="flex items-center">
-              <h4 class="mr-6 text-lg font-bold text-[rgba(133,133,134,1)]">01</h4>
-              <img class="h-20 w-20 rounded-sm" src="images/trackCover.jpg" alt="song image" />
-            </div>
-            <div class="ml-5 capitalize text-[rgba(154,154,154,1)]">
-              <h3 class="font-bold text-[rgb(214,214,214)]">Millions</h3>
-              <p>Always never</p>
-            </div>
-            <div
-              class="ml-auto mr-16 flex items-center gap-16 capitalize text-[rgba(154,154,154,1)]"
-            >
-              <p>Always never</p>
-              <p>8.069 542</p>
-              <p>3:58</p>
-              <button><img class="mb-0.5 h-6 w-6" src="icons/like.svg" alt="like image" /></button>
-            </div>
-          </div>
-          <div class="mt-9 flex items-center">
-            <div class="flex items-center">
-              <h4 class="mr-6 text-lg font-bold text-[rgba(133,133,134,1)]">01</h4>
-              <img class="h-20 w-20 rounded-sm" src="images/trackCover.jpg" alt="song image" />
-            </div>
-            <div class="ml-5 capitalize text-[rgba(154,154,154,1)]">
-              <h3 class="font-bold text-[rgb(214,214,214)]">Millions</h3>
-              <p>Always never</p>
-            </div>
-            <div
-              class="ml-auto mr-16 flex items-center gap-16 capitalize text-[rgba(154,154,154,1)]"
-            >
-              <p>Always never</p>
-              <p>8.069 542</p>
-              <p>3:58</p>
-              <button><img class="mb-0.5 h-6 w-6" src="icons/like.svg" alt="like image" /></button>
-            </div>
-          </div>
-          <div class="mt-9 flex items-center">
-            <div class="flex items-center">
-              <h4 class="mr-6 text-lg font-bold text-[rgba(133,133,134,1)]">01</h4>
-              <img class="h-20 w-20 rounded-sm" src="images/trackCover.jpg" alt="song image" />
-            </div>
-            <div class="ml-5 capitalize text-[rgba(154,154,154,1)]">
-              <h3 class="font-bold text-[rgb(214,214,214)]">Millions</h3>
-              <p>Always never</p>
-            </div>
-            <div
-              class="ml-auto mr-16 flex items-center gap-16 capitalize text-[rgba(154,154,154,1)]"
-            >
-              <p>Always never</p>
-              <p>8.069 542</p>
-              <p>3:58</p>
-              <button><img class="mb-0.5 h-6 w-6" src="icons/like.svg" alt="like image" /></button>
-            </div>
-          </div>
-          <div class="mt-9 flex items-center">
+        <div
+          class="mt-9 max-h-[calc(100%-5rem)] snap-y snap-end snap-always overflow-y-auto [mask-image:linear-gradient(180deg,black_0%,black_75%,transparent_100%)]"
+          @scroll="changeFadeOnScroll($event, true)"
+        >
+          <div v-for="n in 10" class="mb-9 flex items-center">
             <div class="flex items-center">
               <h4 class="mr-6 text-lg font-bold text-[rgba(133,133,134,1)]">01</h4>
               <img class="h-20 w-20 rounded-sm" src="images/trackCover.jpg" alt="song image" />
@@ -151,86 +72,12 @@
         <div
           id="favSongs"
           class="scrollbar-hide mt-9 flex w-full grow-0 flex-nowrap gap-9 overflow-x-auto scroll-smooth pb-2 [mask-image:linear-gradient(90deg,black_0%,black_65%,transparent_100%)]"
-          @wheel="horizontalScroll($event)"
+          @wheel="
+            horizontalScroll($event, 150);
+            changeFadeOnScroll($event, false);
+          "
         >
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
-            <div class="relative h-32 w-32">
-              <img
-                class="absolute top-0 left-0 h-full w-full object-cover"
-                src="images/trackCover.jpg"
-                alt="song cover"
-              />
-            </div>
-            <h3 class="mt-4 font-bold">Alone With You</h3>
-            <p class="text-[rgba(133,133,134,1)]">Alina Baraz</p>
-          </div>
-          <div>
+          <div v-for="n in 20">
             <div class="relative h-32 w-32">
               <img
                 class="absolute top-0 left-0 h-full w-full object-cover"
@@ -244,6 +91,9 @@
         </div>
       </section>
     </main>
+    <!-- <aside class="flex shrink-0">
+      <img src="./icons/close.svg" alt="close active playlist" />
+    </aside> -->
   </div>
 </template>
 
@@ -303,13 +153,36 @@ function debounce(func, waitTime = 250) {
   };
 }
 
-const horizontalScroll = debounce((e, ...args) => {
+function horizontalScroll(e, speed = 200) {
+  e.preventDefault();
   if (e.deltaY > 0) {
-    e.currentTarget.scrollBy({ left: +200, behaviour: 'smooth' });
-    e.preventDefault();
+    e.currentTarget.scrollBy({ left: +speed, behaviour: 'smooth' });
   } else {
-    e.currentTarget.scrollBy({ left: -200, behaviour: 'smooth' });
-    e.preventDefault();
+    e.currentTarget.scrollBy({ left: -speed, behaviour: 'smooth' });
   }
-}, 350);
+}
+
+function changeFadeOnScroll(e, isVertical = true) {
+  let isFullyScrolled;
+  let scrolledAmount = isVertical ? e.currentTarget.scrollTop : e.currentTarget.scrollLeft;
+  let elementSize = isVertical ? e.currentTarget.clientHeight : e.currentTarget.clientWidth;
+  let fullSize = isVertical ? e.currentTarget.scrollHeight : e.currentTarget.scrollWidth;
+
+  isFullyScrolled = scrolledAmount + elementSize >= fullSize;
+
+  let fadePercentage =
+    75 + Math.floor((((scrolledAmount * 100) / (fullSize - elementSize)) * 25) / 100);
+  let gradientDegrees = isVertical ? 180 : 90;
+
+  console.log(isFullyScrolled);
+  console.log(fadePercentage);
+  if (isFullyScrolled) {
+    e.currentTarget.setAttribute('style', 'mask-image:inherit');
+  } else {
+    e.currentTarget.setAttribute(
+      'style',
+      `mask-image:linear-gradient(${gradientDegrees}deg,black 0%,black ${fadePercentage}%,transparent 100%)`
+    );
+  }
+}
 </script>
